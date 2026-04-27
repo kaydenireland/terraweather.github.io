7fc70c6747ebfaf9d5ca91d0bed6a929f1e01767
Kayden Ireland <kaydenireland@aol.com>
2026-04-26 23:43:12 -0400

    Initial Leaflet Map

---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import L from 'leaflet';
+import { createMap } from './map.ts';
+
+const map: L.Map = createMap('map');
---
+++ src/map.ts
@@ -0,0 +1,13 @@
+import L from 'leaflet';
+
+export function createMap(containerId: string): L.Map {
+    const map = L.map(containerId).setView([20, 0], 2);
+
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+        attribution: '© OpenStreetMap © CartoDB',
+        subdomains: 'abcd',
+        maxZoom: 19,
+    }).addTo(map);
+
+    return map;
+}
